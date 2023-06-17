1c84ebdcb6f277cc56abc023b032efcc8a88cbc9
Peakky <131934114+Peakky@users.noreply.github.com>
2023-06-17 13:48:37 +0700

    s

--- core/src/com/mygdx/game/asset/Asset.java
+++ core/src/com/mygdx/game/asset/Asset.java
@@ -9,6 +9,9 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 
 public class Asset {
 
